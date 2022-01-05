4577867ff07153bda80de21dcc1eb9fb7e95c56e
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-01-05 15:09:07 -0300

    Project Start

---
+++ src/application/Program.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("After the game, the king and the pawn go into the same box.");
+    }
+
+}
