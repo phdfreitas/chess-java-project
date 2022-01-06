88003fd254fdbbdaaaa27b8096c0eea78c94d4ca
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-01-06 13:18:24 -0300

    Class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("After the game, the king and the pawn go into the same box.");
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
     }
 
 }
